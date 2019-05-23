5e80be0812d3f48ca3c2e1c0656f669b8dfdeb0d
lucasgsfelix <lucasgsfelix@gmail.com>
2019-05-23 14:54:38 -0300

    Comentários ginástica (ainda não finalizado)

--- 2664 -Ginástica/ginastica.cpp
+++ 2664 -Ginástica/ginastica.cpp
@@ -5,13 +5,17 @@
 using namespace std;
 
 class dados{
+	// Classe responsável por armazenar os dados
 	public:
-		long long int t;
-		long long int m;
-		long long int n;
+		long long int t; // tempo que será realizado essas atividades
+		long long int m; // dificuldade mínima
+		long long int n; // dificuldade máxima
 };
 void leitura(dados *d)
 {
+	/*
+		Método responsável por realizar a leitura de dados
+	*/
 	long long int valor;
 	cin >> valor;
 	d->t = valor; // tamanho da combinação, nº de minutos
@@ -20,8 +24,11 @@ void leitura(dados *d)
 	cin >> valor;
 	d->n = valor; // valor de dificuldade máxima
 }
-long long int ** alocar_matriz(long long int linhas, long long int colunas){ //Recebe a quantidade de linhas e colunas como Parâmetro
-  	
+long long int ** alocar_matriz(long long int linhas, long long int colunas)
+{
+ 	/*
+		Método responsável por realizar a alocação dinâmica de uma matriz
+ 	*/ 	
   	long long int i,j;
 	long long int **m = (long long int**)malloc(linhas * sizeof(long long int*)); //Aloca um Vetor de Ponteiros
 	for (i = 0; i < linhas; i++)
@@ -36,10 +43,33 @@ long long int ** alocar_matriz(long long int linhas, long long int colunas){ //R
 }
 int ginastica_pd(dados *d)
 {
+	/*
+		Para a resolução problema da ginastica foi utilizado um método
+		programação dinâmica.
+
+		O método implementado calculo a quantidade possíveis combinações
+		para cada instante de tempo. Este método se mostrou mais apropriado
+		que a resolução apenas por recursão (indução), pois armazena os valores
+		na matriz. Desta maneira, não era necessário o recalculo para atingir um
+		determinado valor tornando o tempo de execução menor.
+
+		A complexidade deste algoritmo é O(t*tamanho_coluna). Onde t é a quantidade
+		de tempo que irá ser feito uma atividade e o tamanho da coluna resume a
+		quantidade de atividades totais que podem ser realizadas em determinado
+		período de tempo.
+
+	*/
 	long long int i, j, tamanho_coluna = d->n - d->m + 1, soma = 0;
 	long long int iter_aux_i = 0, iter_aux_j = 1, aux = 0;
-	long long int **m = alocar_matriz(d->t, tamanho_coluna);
+
+	long long int **m = alocar_matriz(d->t, tamanho_coluna); // chama a alocação de matrizes
+	/*
+		Calcula o mod para dividir os números 
+		os quais podem ser muito grandes
+		tornando inviável o cálculo simples.
+	*/
 	long long int mod = pow(10, 9) + 7;
+
 	for(i=1;i<d->t;i++)
 	{
 		for(j=1;j<tamanho_coluna;j++)
@@ -71,8 +101,13 @@ int main()
 	
 	if(! d.t < 1 or d.t>50 or d.m < 1 or d.n < 1 or d.m > pow(10, 5) or d.n > pow(10, 5))
 	{
-		int s = ginastica_pd(&d);
-		cout << s << "\n";
+		/*
+			Avalia para que não passe parâmetros inválidos
+		*/
+		
+		int s = ginastica_pd(&d); // chamada para instância a programação dinâmica para este problema
+		
+		cout << s << "\n"; // imprime solução final
 	}
 		
 
