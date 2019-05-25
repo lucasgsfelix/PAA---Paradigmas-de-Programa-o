e28e3c5a4b3feebcf39235e78706d2a58eddd87b
lucasgsfelix <lucasgsfelix@gmail.com>
2019-05-25 00:45:02 -0300

    Updating planta casa

--- 2796 - Planta da Casa/planta_casa.cpp
+++ 2796 - Planta da Casa/planta_casa.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -29,22 +30,38 @@ class casa
 		int comprimento_casa = 0;
 		int largura_casa = 0;
 		int quantidade_mesas = 0;
-		char **m;
+		int **m;
+		int **s; //irá armazenar a soma
 		vector <mesa> mesas;
 };
 
-void alocar_matriz(int linhas, int colunas, casa *c)
+void alocar_matriz(int linhas, int colunas, casa *c, int flag)
 {
  	/*
 		Método responsável por realizar a alocação dinâmica de uma matriz
  	*/ 	
-	c->m = (char**)malloc(linhas * sizeof(char*)); //Aloca um Vetor de Ponteiros
+ 	if(flag == 0)
+ 	{
+		c->m = (int**)malloc(linhas * sizeof(int*)); //Aloca um Vetor de Ponteiros	
+ 	}
+ 	else
+ 	{
+ 		c->s = (int**)malloc(linhas * sizeof(int*)); //Aloca um Vetor de Ponteiros
+ 	}
 	for (int i = 0; i < linhas; i++)
 	{
 		/*
 			Aloca um Vetor de Inteiros para cada posição do Vetor de Ponteiros.
 		*/
-	  	c->m[i] = (char*) malloc(colunas * sizeof(char));
+		if(flag == 0)
+		{
+			c->m[i] = (int*) malloc(colunas * sizeof(int));
+		}
+		else
+		{
+			c->s[i] = (int*) malloc(colunas * sizeof(int));
+		}
+	  	
 	}
 
 }
@@ -67,7 +84,7 @@ void leitura_montagem_casa(vector <char> *buffer, casa *c)
 	/*
 		Alocando a matriz que será a casa
 	*/
-	alocar_matriz(c->comprimento_casa, c->largura_casa, c);
+	alocar_matriz(c->comprimento_casa, c->largura_casa, c, 0);
 	while(i < c->largura_casa * c->comprimento_casa)
 	{
 		/*
@@ -75,8 +92,15 @@ void leitura_montagem_casa(vector <char> *buffer, casa *c)
 
 		*/
 		cin >> valor_aux;
-		c->m[k][j] = valor_aux;
-		
+		if(valor_aux == '#')
+		{
+			c->m[k][j] = 0;
+		}
+		else if(valor_aux=='.')
+		{
+			c->m[k][j] =1;
+		}
+		//cout << c->m[k][j] << ",";
 		i++;
 		j++;
 		if(j == c->largura_casa)
@@ -101,12 +125,83 @@ void leitura_montagem_casa(vector <char> *buffer, casa *c)
 	}
 
 }
+int calcula_subareas_contiguas(casa *c)
+{
+	/*
+		Função responsável por calcular as 
+		maiores subáreas contíguas que a casa possui,
+		pois é lá que a mesa irá ficar.
+		Este problema pode ser resolvido com programação dinâmica
+		https://www.geeksforgeeks.org/maximum-size-rectangle-binary-sub-matrix-1s/
+		Neste site a base de como foi calculado as subáreas contíguas
+	*/
+	int i, j;
+
+	alocar_matriz(c->comprimento_casa, c->largura_casa, c, 1); // alocando a matriz de soma
+	/*
+		Inicializando primeira linha e primeira coluna
+	*/
+	for(i=0;i<c->comprimento_casa;i++)
+	{
+		c->s[i][0] = c->m[i][0]; //copiando a primeira coluna
+	}
+	for(i=0;i<c->largura_casa;i++)
+	{
+		c->s[0][i] = c->m[0][i]; //copiando a primeira linha
+	}
+
+
+	for(i=1;i<c->comprimento_casa;i++)
+	{
+		for(j=1;j<c->largura_casa;j++)
+		{
+			if(c->m[i][j] == 1)
+			{
+				//c->s[i][j] = min(c->s[i][j-1], min(c->s[i-1][j], c->s[i-1][j-1])) + 1;
+				c->s[i][j] += c->s[i-1][j] + 1;
+			}
+			else
+			{
+				c->s[i][j] = 0;
+			}
+			cout << c->s[i][j];
+		}
+		cout << "\n";
+	}
+
+	/*
+		Selecionando e salvando o melhor valor e index das linhas
+	*/
+	int best_i, best_j, local_best = c->s[0][0];
+	for(i=0;i<c->comprimento_casa;i++)
+	{
+		for(j=0;j<c->largura_casa;j++)
+		{
+			if(local_best < c->s[i][j])
+			{
+				local_best = c->s[i][j];
+				best_i = i;
+				best_j = j;
+			}
+		}
+	}
+	cout << "Local Best: " << local_best << "\n";
+	cout << best_i << " " << best_i - local_best << "\n";
+	int comprimento = best_i - (best_i-local_best);
+	cout << comprimento << "\n";
+	cout << best_j << " " << best_j - local_best << "\n";
+	int largura = best_j - (best_j - local_best);
+	cout << largura << "\n";
+	
+	return 0;
+}
 int main()
 {
 	casa c; // objeto que irá armazenar a casa da entrada
 	vector <char> buffer;
 	leitura_montagem_casa(&buffer, &c);
 	buffer.clear();//limpando buffer
+	calcula_subareas_contiguas(&c);
 
 
 	return 0;
